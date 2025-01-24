b7fd7700c423decdc5e2a94d3f9ff41cacb4a1c3
Andrew <murdademall@MacBook-Air-Andrew.local>
2025-01-24 14:26:49 +0700

    fixing issues with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simple-todo-app',
+  base: '/todo-app-test',
   test: {
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
